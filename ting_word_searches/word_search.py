8cad0530f516c73c07d99c24374f984da7e10186
Gabriel Duarte <gabrielduarte1405@gmail.com>
2023-06-13 17:17:38 -0300

    Requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    words = list()
+    ocorrencias = list()
+    for index in range(len(instance)):
+        for index2, line in enumerate(
+            instance.search(index)["linhas_do_arquivo"]
+        ):
+            if word.lower() in line.lower():
+                ocorrencias.append({"linha": index2 + 1})
+        if ocorrencias:
+            words.append(
+                {
+                    "palavra": word,
+                    "arquivo": instance.search(index)["nome_do_arquivo"],
+                    "ocorrencias": ocorrencias,
+                }
+            )
+    return words
 
 
 def search_by_word(word, instance):
