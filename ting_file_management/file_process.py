7bea43cb0035bb2238c050d403b9a62d870c67a6
Gabriel Duarte <gabrielduarte1405@gmail.com>
2023-06-13 16:33:55 -0300

    Requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(0, len(instance)):
+        if instance.search(index)['nome_do_arquivo'] == path_file:
+            return None
+    files = txt_importer(path_file)
+    dictionary = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(files),
+        'linhas_do_arquivo': files,
+    }
+    instance.enqueue(dictionary)
+    print(dictionary, file=sys.stdout)
 
 
 def remove(instance):
